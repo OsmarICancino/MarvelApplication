70248d68591f4a9c1a9f6144027163ff6ae585b4
Osmar ICancino <o.cancinodiaz@gmail.com>
2019-04-13 01:52:52 -0500

    Initial commit

---
+++ Marvel/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Marvel
+//
+//  Created by Osmar Ismael Cancino Díaz on 4/13/19.
+//  Copyright © 2019 Osmar Ismael Cancino Díaz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
